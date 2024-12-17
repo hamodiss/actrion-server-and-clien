de582c0965ce426ed9ea553bb65dcfb6e1a6f835
hamodiss <131769846+hamodiss@users.noreply.github.com>
2024-12-17 12:01:46 +0100

    Delete ugv_ws directory

--- ugv_ws/build/ugv_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/momo/ugv_ws/src/ugv_control
-# Build directory: /home/momo/ugv_ws/build/ugv_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
